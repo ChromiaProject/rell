b72d89875217596e31b6befdc294ee65a8851758
Joar Svartholm <joar.svartholm@netlight.com>
2022-07-04 21:57:09 +0200

    Clean up unit test

--- kotlin-codegen/src/test/kotlin/net/postchain/rell/codegen/kotlin/KotlinEntityTest.kt
+++ kotlin-codegen/src/test/kotlin/net/postchain/rell/codegen/kotlin/KotlinEntityTest.kt
@@ -12,21 +12,34 @@ import java.io.File
 
 import assertk.assert
 import assertk.assertions.contains
+import net.postchain.rell.model.R_Module
+import org.junit.jupiter.api.BeforeAll
+import kotlin.test.assertNotNull
 
 internal class KotlinEntityTest {
 
+    companion object {
+
+        lateinit var testModule: R_Module
+        @JvmStatic
+        @BeforeAll
+        fun compileTestApp() {
+            testModule = RellCliUtils.compileApp(
+                C_SourceDir.diskDir(File(javaClass.getResource("entity.rell")!!.toURI()).parentFile),
+                C_CompilerModuleSelection(
+                    listOf(R_ModuleName.of("entity"))
+                ),
+                true,
+                C_CompilerOptions.DEFAULT
+            ).let {
+                assertNotNull(it.moduleMap[R_ModuleName.of("entity")])
+            }
+        }
+    }
+
     @Test
     fun format() {
-        val a = RellCliUtils.compileApp(
-            C_SourceDir.diskDir(File(javaClass.getResource("entity.rell")!!.toURI()).parentFile),
-            C_CompilerModuleSelection(
-                listOf(R_ModuleName.of("entity"))
-            ),
-            true,
-            C_CompilerOptions.DEFAULT
-        )
-
-        val entity = a.moduleMap[R_ModuleName.of("entity")]!!.entities.values.first()
+        val entity = assertNotNull(testModule.entities["test_entity"])
         val k = KotlinEntity(entity)
         val formatted = k.format()
         assert(formatted).all {
