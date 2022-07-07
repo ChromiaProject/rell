638807ad86a3c3dc49d73a861391ff0fde17e9d9
Joar Svartholm <joar.svartholm@netlight.com>
2022-07-07 12:09:17 +0200

    Add basic tasks to run maven from gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,26 @@
-version="0.2.0-SNAPSHOT"
-group="net.postchain.rell"
+version = "0.2.0-SNAPSHOT"
+group = "net.postchain.rell"
+
+
+val mavenPlugin = tasks.register<Exec>("buildMavenPlugin") {
+    description = "Builds the maven plugin"
+    group = "build"
+    commandLine("mvn", "clean", "test", "package")
+    workingDir("$projectDir/plugin/rellgen-maven-plugin")
+}
+
+val mavenPluginInstall = tasks.register<Exec>("publishMavenPluginToMavenLocal") {
+    description = "Installs the maven plugin to local repository"
+    group = "publishing"
+    dependsOn("buildMavenPlugin")
+    commandLine("mvn", "install")
+    workingDir("$projectDir/plugin/rellgen-maven-plugin")
+}
+
+val mavenPluginDeploy by tasks.register<Exec>("publishMavenPlugin") {
+    description = "Deploys maven plugin"
+    group = "publishing"
+    dependsOn("publishMavenPluginToMavenLocal")
+    commandLine("mvn", "deploy:deploy")
+    workingDir("$projectDir/plugin/rellgen-maven-plugin")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,5 +4,5 @@ include("rellgen")
 findProject(":rellgen")?.name = "rellgen"
 include("codegen")
 include("codegen-kotlin")
+include(":plugin")
 include("plugin:rellgen-gradle-plugin")
-findProject(":plugin:rellgen-gradle-plugin")?.name = "rellgen-gradle-plugin"
