b5193776be326531dd8b2011467dd4e0d1e793e7
Mikael Ståldal <mikael.staldal@chromaway.com>
2022-08-23 16:31:07 +0200

    Compile generated Kotlin code

--- codegen-kotlin/src/test/kotlin/net/postchain/rell/codegen/kotlin/KotlinCodeGeneratorTest.kt
+++ codegen-kotlin/src/test/kotlin/net/postchain/rell/codegen/kotlin/KotlinCodeGeneratorTest.kt
@@ -2,25 +2,28 @@ package net.postchain.rell.codegen.kotlin
 
 import assertk.assertions.contains
 import assertk.assertions.hasSize
+import assertk.assertions.isEqualTo
 import net.postchain.rell.codegen.CodeGenerator
-import net.postchain.rell.codegen.document.Document
-import net.postchain.rell.codegen.document.DocumentFactory
-import net.postchain.rell.codegen.section.*
-import net.postchain.rell.compiler.base.core.C_CompilerModuleSelection
-import net.postchain.rell.compiler.base.core.C_CompilerOptions
-import net.postchain.rell.compiler.base.utils.C_SourceDir
-import net.postchain.rell.model.*
-import net.postchain.rell.utils.RellCliUtils
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeAll
+import net.postchain.rell.codegen.document.DocumentFile
+import net.postchain.rell.codegen.document.DocumentSaver
+import net.postchain.rell.codegen.section.DocumentSection
+import org.jetbrains.kotlin.cli.common.ExitCode
+import org.jetbrains.kotlin.cli.common.arguments.K2JVMCompilerArguments
+import org.jetbrains.kotlin.cli.common.messages.MessageRenderer
+import org.jetbrains.kotlin.cli.common.messages.PrintingMessageCollector
+import org.jetbrains.kotlin.cli.jvm.K2JVMCompiler
+import org.jetbrains.kotlin.config.Services
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.io.File
+import java.nio.file.Files
+import java.util.stream.Collectors
 
 internal class CodeGeneratorTest {
 
     val generator = CodeGenerator(KotlinDocumentFactory("com.example"))
     lateinit var sections: List<DocumentSection>
+    lateinit var documents: List<DocumentFile>
 
     @BeforeEach
     fun setup() {
@@ -28,6 +31,33 @@ internal class CodeGeneratorTest {
             File(this::class.java.getResource("multi/a/module.rell")!!.toURI()).parentFile.parentFile,
             "a",
         )
+        documents = generator.constructDocuments(sections, true)
+        val target = Files.createTempDirectory("rell-codegen")
+        DocumentSaver(target.toFile()).saveDocuments(documents)
+
+        val classes = Files.createTempDirectory("rell-codegen-classes")
+        K2JVMCompiler().run {
+            val args = K2JVMCompilerArguments().apply {
+                freeArgs = documents.stream().map(DocumentFile::path).map { "$target/$it" }.collect(Collectors.toList())
+                destination = classes.toAbsolutePath().toString()
+                classpath = System.getProperty("java.class.path")
+                    .split(System.getProperty("path.separator"))
+                    .filter {
+                        File(it).exists() && File(it).canRead()
+                    }.joinToString(":")
+                noStdlib = true
+                noReflect = true
+                skipRuntimeVersionCheck = true
+                jvmTarget = "11"
+            }
+            val exitCode: ExitCode = execImpl(
+                PrintingMessageCollector(
+                    System.out,
+                    MessageRenderer.WITHOUT_PATHS, false),
+                Services.EMPTY,
+                args)
+            assertk.assert(exitCode.code).isEqualTo(0)
+        }
     }
 
     @Test
@@ -37,7 +67,6 @@ internal class CodeGeneratorTest {
 
     @Test
     fun documents() {
-        val documents = generator.constructDocuments(sections, true)
         assertk.assert(documents).hasSize(5)
         assertk.assert(documents[1].document.format()).contains("import com.example.RootStruct")
         assertk.assert(documents[1].document.format()).contains("import com.example.b.BStruct")
