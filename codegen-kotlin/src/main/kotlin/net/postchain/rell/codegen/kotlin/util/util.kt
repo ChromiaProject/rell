830680f8a9672ea181843c5c8ad0d208163fa888
Mikael Ståldal <mikael.staldal@chromaway.com>
2022-10-18 12:47:42 +0000

    Use GtvObjectMapper to encode structs

--- codegen-kotlin/src/main/kotlin/net/postchain/rell/codegen/kotlin/util/util.kt
+++ codegen-kotlin/src/main/kotlin/net/postchain/rell/codegen/kotlin/util/util.kt
@@ -6,7 +6,7 @@ import net.postchain.rell.model.*
 fun attributeToGtv(name: String, type: R_Type): String {
     return when (type) {
         is R_EnumType -> "gtv(${name}.ordinal.toLong())"
-        is R_StructType -> "$name.toGtv()"
+        is R_StructType -> "GtvObjectMapper.toGtvArray($name)"
         is R_DecimalType -> "gtv($name.toString())"
         is R_NullableType -> "$name.let { if (it == null) GtvNull else ${attributeToGtv("it", type.valueType)} }"
         is R_ListType -> "gtv($name.map { ${attributeToGtv("it", type.elementType)} })"
