79bd012d466fcd0660a821be289f95b4fa9767a1
Mikael Ståldal <mikael.staldal@chromaway.com>
2023-02-10 09:31:05 +0100

    Fix review comment

--- codegen-kotlin/src/main/kotlin/net/postchain/rell/codegen/kotlin/util/util.kt
+++ codegen-kotlin/src/main/kotlin/net/postchain/rell/codegen/kotlin/util/util.kt
@@ -3,20 +3,18 @@ package net.postchain.rell.codegen.kotlin.util
 import net.postchain.rell.codegen.deps.CamelCaseClassName
 import net.postchain.rell.model.*
 
-fun attributeToGtv(name: String, type: R_Type): String {
-    return aliasToGtv(name, type) ?: when (type) {
-        is R_EnumType -> "gtv(${name}.ordinal.toLong())"
-        is R_StructType -> "GtvObjectMapper.toGtvArray($name)"
-        is R_DecimalType -> "gtv($name.toString())"
-        is R_RowidType -> "gtv($name.id)"
-        is R_EntityType -> "gtv($name.id)"
-        is R_NullableType -> "$name.let { if (it == null) GtvNull else ${attributeToGtv("it", type.valueType)} }"
-        is R_ListType -> "gtv(${nestedAttributeToGtv(name, type.elementType, false)})"
-        is R_SetType -> "gtv(${nestedAttributeToGtv(name, type.elementType, true)})"
-        is R_MapType -> mapTypeToGtv(name, type)
-        is R_GtvType -> name
-        else -> "gtv($name)"
-    }
+fun attributeToGtv(name: String, type: R_Type): String = aliasToGtv(name, type) ?: when (type) {
+    is R_EnumType -> "gtv(${name}.ordinal.toLong())"
+    is R_StructType -> "GtvObjectMapper.toGtvArray($name)"
+    is R_DecimalType -> "gtv($name.toString())"
+    is R_RowidType -> "gtv($name.id)"
+    is R_EntityType -> "gtv($name.id)"
+    is R_NullableType -> "$name.let { if (it == null) GtvNull else ${attributeToGtv("it", type.valueType)} }"
+    is R_ListType -> "gtv(${nestedAttributeToGtv(name, type.elementType, false)})"
+    is R_SetType -> "gtv(${nestedAttributeToGtv(name, type.elementType, true)})"
+    is R_MapType -> mapTypeToGtv(name, type)
+    is R_GtvType -> name
+    else -> "gtv($name)"
 }
 
 private fun nestedAttributeToGtv(name: String, type: R_Type, isSet: Boolean): String {
@@ -32,38 +30,33 @@ private fun mapTypeToGtv(name: String, type: R_MapType) = when (type.keyType) {
     else -> "gtv($name.map { (k, v) -> gtv(${attributeToGtv("k", type.keyType)}, ${attributeToGtv("v", type.valueType)}) })"
 }
 
-fun rTypeToString(name: String, type: R_Type, primitiveTypes: Boolean, aliases: Boolean): String {
-    return aliasToString(name, type, aliases) ?: when (type) {
-        is R_NullableType -> "${rTypeToString(name, type.valueType, primitiveTypes, aliases)}?"
-        is R_BooleanType -> "Boolean"
-        is R_IntegerType -> "Long"
-        is R_DecimalType -> "BigDecimal"
-        is R_TextType -> "String"
-        is R_ByteArrayType -> if (primitiveTypes) "ByteArray" else "WrappedByteArray"
-        is R_RowidType -> "RowId"
-        is R_EntityType -> "RowId"
-        is R_JsonType -> "String"
-        is R_SetType -> "Set<${rTypeToString(name, type.elementType, primitiveTypes, aliases)}>"
-        is R_ListType -> "List<${rTypeToString(name, type.elementType, primitiveTypes, aliases)}>"
-        is R_MapType -> "Map<${rTypeToString(name, type.keyType, primitiveTypes, aliases)}, ${rTypeToString(name, type.valueType, primitiveTypes, aliases)}>"
-        is R_StructType -> CamelCaseClassName.fromRellType(type).name
-        is R_EnumType -> CamelCaseClassName.fromRellType(type).name
-        else -> "Gtv"
-    }
+fun rTypeToString(name: String, type: R_Type, primitiveTypes: Boolean, aliases: Boolean): String =
+        aliasToString(name, type, aliases) ?: when (type) {
+            is R_NullableType -> "${rTypeToString(name, type.valueType, primitiveTypes, aliases)}?"
+            is R_BooleanType -> "Boolean"
+            is R_IntegerType -> "Long"
+            is R_DecimalType -> "BigDecimal"
+            is R_TextType -> "String"
+            is R_ByteArrayType -> if (primitiveTypes) "ByteArray" else "WrappedByteArray"
+            is R_RowidType -> "RowId"
+            is R_EntityType -> "RowId"
+            is R_JsonType -> "String"
+            is R_SetType -> "Set<${rTypeToString(name, type.elementType, primitiveTypes, aliases)}>"
+            is R_ListType -> "List<${rTypeToString(name, type.elementType, primitiveTypes, aliases)}>"
+            is R_MapType -> "Map<${rTypeToString(name, type.keyType, primitiveTypes, aliases)}, ${rTypeToString(name, type.valueType, primitiveTypes, aliases)}>"
+            is R_StructType -> CamelCaseClassName.fromRellType(type).name
+            is R_EnumType -> CamelCaseClassName.fromRellType(type).name
+            else -> "Gtv"
+        }
+
+fun aliasToString(name: String, type: R_Type, enabled: Boolean): String? = if (!enabled) null else when {
+    enabled && name == "pubkey" && type is R_ByteArrayType -> "PubKey"
+    enabled && name == "blockchain_rid" && type is R_ByteArrayType -> "BlockchainRid"
+    else -> null
 }
 
-fun aliasToString(name: String, type: R_Type, aliases: Boolean): String? {
-    return when {
-        aliases && name == "pubkey" && type is R_ByteArrayType -> "PubKey"
-        aliases && name == "blockchain_rid" && type is R_ByteArrayType -> "BlockchainRid"
-        else -> null
-    }
-}
-
-fun aliasToGtv(name: String, type: R_Type): String? {
-    return when {
-        type is R_NullableType && name == "pubkey" && type.valueType is R_ByteArrayType -> "pubkey.let { if (it == null) GtvNull else gtv(it.data) }"
-        name == "pubkey" && type is R_ByteArrayType -> "gtv(pubkey.data)"
-        else -> null
-    }
+fun aliasToGtv(name: String, type: R_Type): String? = when {
+    type is R_NullableType && name == "pubkey" && type.valueType is R_ByteArrayType -> "pubkey.let { if (it == null) GtvNull else gtv(it.data) }"
+    name == "pubkey" && type is R_ByteArrayType -> "gtv(pubkey.data)"
+    else -> null
 }
