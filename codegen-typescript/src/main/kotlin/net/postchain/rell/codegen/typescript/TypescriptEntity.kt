e021ff4673e4430dd8d8a0d85ca895d877f6f9c2
Tim Steinholtz <tim.steinholtz@netlight.com>
2023-02-27 15:20:37 +0100

    Reverts push to dev

--- codegen-typescript/src/main/kotlin/net/postchain/rell/codegen/typescript/TypescriptEntity.kt
+++
@@ -1,8 +0,0 @@
-package net.postchain.rell.codegen.typescript
-
-import net.postchain.rell.codegen.deps.ClassName
-import net.postchain.rell.codegen.section.Struct
-import net.postchain.rell.model.R_EntityDefinition
-
-class TypescriptEntity(className: ClassName, entity: R_EntityDefinition) :
-        DataTypeSection(className, entity.attributes.values.associateBy({ it.name }, { it.type })), Struct
