ea871ae7d4bc9fad2666e7c00e4eb992f0f6ed3e
Tim Steinholtz <tim.steinholtz@netlight.com>
2023-02-27 14:25:34 +0100

    Adds Typescript Entity

---
+++ codegen-typescript/src/main/kotlin/net/postchain/rell/codegen/typescript/TypescriptEntity.kt
@@ -0,0 +1,8 @@
+package net.postchain.rell.codegen.typescript
+
+import net.postchain.rell.codegen.deps.ClassName
+import net.postchain.rell.codegen.section.Struct
+import net.postchain.rell.model.R_EntityDefinition
+
+class TypescriptEntity(className: ClassName, entity: R_EntityDefinition) :
+        DataTypeSection(className, entity.attributes.values.associateBy({ it.name }, { it.type })), Struct
