956d1a3f998f8d9a325c2e8ee89b396ae1a37459
Joar Svartholm <joar.svartholm@chromaway.com>
2023-05-03 10:02:28 +0200

    Use typesafe api for dependencies

--- codegen/build.gradle.kts
+++ codegen/build.gradle.kts
@@ -2,16 +2,8 @@ plugins {
     id("net.postchain.rell.codegen.kotlin-library-conventions")
 }
 
-repositories {
-    mavenCentral()
-    maven("https://gitlab.com/api/v4/projects/32294340/packages/maven")
-    maven("https://gitlab.com/api/v4/projects/32802097/packages/maven")
-    maven("https://jcenter.bintray.com")
-    maven("https://maven.emrld.io")
-}
-
 dependencies {
-    implementation("net.postchain.rell:rell:0.12.1")
+    implementation(libs.rell)
 }
 
 val testConfiguration by configurations.creating
