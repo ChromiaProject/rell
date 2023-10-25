2d0478e5fcd53d7a36fbaf68588dabcfb9b30d05
Tim Steinholtz <tim.steinholtz@chromaway.com>
2023-10-25 16:07:36 +0200

    Redirect standarnd in and out for stdio launcher

--- language-server/src/main/kotlin/net/postchain/rell/toolbox/lsp/launcher/ServerLauncher.kt
+++ language-server/src/main/kotlin/net/postchain/rell/toolbox/lsp/launcher/ServerLauncher.kt
@@ -3,9 +3,8 @@ package net.postchain.rell.toolbox.lsp.launcher
 import io.github.oshai.kotlinlogging.KotlinLogging
 import net.postchain.rell.toolbox.lsp.server.RellLanguageServer
 import org.eclipse.lsp4j.launch.LSPLauncher
-import java.io.InputStream
-import java.io.OutputStream
-import java.io.PrintWriter
+import java.io.*
+
 
 class ServerLauncher(
     private val serverInputStream: InputStream,
@@ -26,6 +25,7 @@ class ServerLauncher(
                 validate,
                 trace
             )
+            redirectStandardStreams(trace)
             languageServer.connect(launcher.remoteProxy)
             launcher.startListening()
             logger.info { "Rell Language Server started." }
@@ -34,4 +34,15 @@ class ServerLauncher(
         }
     }
 
+    private fun redirectStandardStreams(trace: PrintWriter?) {
+        val newOutputStream = if (trace == null) {
+            ByteArrayOutputStream.nullOutputStream()
+        } else {
+            val outputFile = File("output.log")
+            FileOutputStream(outputFile)
+        }
+        val newInputStream = ByteArrayInputStream(byteArrayOf(0))
+        System.setIn(newInputStream)
+        System.setOut(PrintStream(newOutputStream))
+    }
 }
