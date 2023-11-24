af974abcc18b11119d48d65ed0fb764c5ffd8960
Tim Steinholtz <tim.steinholtz@chromaway.com>
2023-11-24 16:36:58 +0100

    Adds test for when resource don't exists

--- language-server/src/test/kotlin/net/postchain/rell/toolbox/lsp/symbols/RellSymbolServiceTest.kt
+++ language-server/src/test/kotlin/net/postchain/rell/toolbox/lsp/symbols/RellSymbolServiceTest.kt
@@ -1,6 +1,7 @@
 package net.postchain.rell.toolbox.lsp.symbols
 
 import assertk.assertThat
+import assertk.assertions.isEmpty
 import assertk.assertions.isEqualTo
 import net.postchain.rell.toolbox.core.indexer.WorkspaceIndexer
 import net.postchain.rell.toolbox.lsp.editing.Document
@@ -20,7 +21,7 @@ class RellSymbolServiceTest {
     }
 
     @Test
-    fun `Returns empty list when symbol does not exist`(@TempDir dir : File) {
+    fun `Returns empty list when resource does not exist`(@TempDir dir: File) {
         val rellFile = File(dir, "rell_file.rell").apply {
             writeText(
                 """
@@ -31,11 +32,33 @@ class RellSymbolServiceTest {
             """.trimIndent()
             )
         }
-        val document = Document(rellFile.toURI(),1, rellFile.readText())
+
         val indexer = WorkspaceIndexer(dir.toURI())
         indexer.initialFileIndexBuild()
-        val position = Position(1,1)
-        val res = rellSymbolService.getSymbolLocations(document,indexer, position)
-        println(res) //HERE I AM
+        val unIndexedFile = File(dir, "unindexed_file.rell").apply { writeText("""""") }
+        val document = Document(unIndexedFile.toURI(), 1, rellFile.readText())
+        val position = Position(1, 1)
+        val res = rellSymbolService.getSymbolLocations(document, indexer, position)
+        assertThat(res).isEmpty()
+    }
+
+    @Test
+    fun `Returns empty list when symbol does not exist`(@TempDir dir: File) {
+        val rellFile = File(dir, "rell_file.rell").apply {
+            writeText(
+                """
+                module;
+                function main() {
+                    return "main";
+                }
+            """.trimIndent()
+            )
+        }
+        val document = Document(rellFile.toURI(), 1, rellFile.readText())
+        val indexer = WorkspaceIndexer(dir.toURI())
+        indexer.initialFileIndexBuild()
+        val position = Position(1, 1)
+        val res = rellSymbolService.getSymbolLocations(document, indexer, position)
+        assertThat(res).isEmpty()
     }
 }
