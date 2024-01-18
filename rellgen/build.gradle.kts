d53cb04a27a5adaa88932596cc9407030cf0796b
Joar Svartholm <joar.svartholm@chromaway.com>
2024-01-18 17:23:49 +0100

    Enable jacoco code coverage reports in gitlab

--- rellgen/build.gradle.kts
+++ rellgen/build.gradle.kts
@@ -6,6 +6,7 @@
 
 plugins {
     id("net.postchain.rell.codegen.kotlin-application-conventions")
+    id("jacoco-report-aggregation")
 }
 
 dependencies {
@@ -22,4 +23,22 @@ dependencies {
 application {
     // Define the main class for the application.
     mainClass.set("net.postchain.rell.codegen.app.AppKt")
+}
+
+reporting {
+    reports {
+        val jacoco by creating(JacocoCoverageReport::class) {
+            testType.set(TestSuiteType.UNIT_TEST)
+        }
+    }
+}
+
+tasks.check {
+    dependsOn(tasks.named<JacocoReport>("testCodeCoverageReport"))
+}
+
+tasks.testCodeCoverageReport {
+    reports {
+    }
+
 }
