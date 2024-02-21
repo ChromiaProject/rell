d02b564f4c4642598146654d5c1cca371cc229c6
Joar Svartholm <joar.svartholm@chromaway.com>
2024-02-21 16:07:01 +0100

    Add custom style sheet and logos

--- src/main/kotlin/com/chromia/rell/dokka/cli/main.kt
+++ src/main/kotlin/com/chromia/rell/dokka/cli/main.kt
@@ -1,16 +1,21 @@
 package com.chromia.rell.dokka.cli
 
+import com.chromia.rell.dokka.RellDokkaPlugin
 import com.chromia.rell.dokka.config.RellConfig
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.multiple
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.options.split
 import com.github.ajalt.clikt.parameters.types.file
+import org.jetbrains.dokka.DokkaConfiguration
 import org.jetbrains.dokka.DokkaConfigurationImpl
 import org.jetbrains.dokka.DokkaGenerator
 import org.jetbrains.dokka.DokkaSourceSetID
 import org.jetbrains.dokka.DokkaSourceSetImpl
+import org.jetbrains.dokka.PluginConfigurationImpl
+import org.jetbrains.dokka.base.DokkaBase
 import org.jetbrains.dokka.utilities.DokkaConsoleLogger
 import org.jetbrains.dokka.utilities.LoggingLevel
 import java.io.File
@@ -21,14 +26,24 @@ class DokkaCommand : CliktCommand() {
     private val target by option().file(canBeFile = false).default(File("out"))
     private val modules by option().split(",")
     private val name by option().default("My Rell Dapp")
+    private val styles by option().default("src/main/resources/styles/styles.css")
+    private val assets by option().split(",").default(listOf("src/main/resources/img/chromia-symbol.png", "src/main/resources/fonts"))
 
     override fun run() {
         val sourceSet = DokkaSourceSetImpl(sourceRoots = setOf(source), sourceSetID = DokkaSourceSetID("main", "dapp"))
+        /*val baseConf = DokkaBaseConfiguration(
+                customStyleSheets = listOf(this::class.java.getResource("/styles.css")!!.toURI().toPath().toFile())
+        )*/
+        val dokkaBaseConf = PluginConfigurationImpl(
+                DokkaBase::class.qualifiedName!!,
+                DokkaConfiguration.SerializationFormat.JSON,
+                """{"customStyleSheets": ["$styles"], "customAssets":[${assets.joinToString(",") { "\"$it\"" }}]}"""
+        )
         val rellConfig = RellConfig(name, modules).toPluginConfig()
         val config = DokkaConfigurationImpl(
                 sourceSets = listOf(sourceSet),
                 outputDir = target,
-                pluginsConfiguration = listOf(rellConfig)
+                pluginsConfiguration = listOf(rellConfig, dokkaBaseConf)
         )
         DokkaGenerator(config, DokkaConsoleLogger(LoggingLevel.DEBUG)).generate()
     }
