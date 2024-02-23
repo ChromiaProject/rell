d97db67cab845731b75eeb6a6e7914507e8ee5a5
Joar Svartholm <joar.svartholm@chromaway.com>
2024-02-23 13:00:48 +0100

    add doc comments to type page

--- src/main/kotlin/com/chromia/rell/dokka/systemlib/namespace_member.kt
+++ src/main/kotlin/com/chromia/rell/dokka/systemlib/namespace_member.kt
@@ -1,6 +1,7 @@
 @file:Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
 package com.chromia.rell.dokka.systemlib
 
+import com.chromia.rell.dokka.doc.toDocumentationNode
 import com.chromia.rell.dokka.translator.RellDeclarationDescriptor
 import net.postchain.rell.base.lmodel.L_NamespaceMember
 import net.postchain.rell.base.lmodel.L_NamespaceMember_Type
@@ -10,8 +11,11 @@ import net.postchain.rell.base.lmodel.L_TypeDefMember_Function
 import net.postchain.rell.base.lmodel.L_TypeDefMember_Property
 import org.jetbrains.dokka.DokkaConfiguration
 import org.jetbrains.dokka.analysis.kotlin.descriptors.compiler.configuration.DescriptorDocumentableSource
+import org.jetbrains.dokka.base.renderers.html.SearchbarDataInstaller
 import org.jetbrains.dokka.links.DRI
+import org.jetbrains.dokka.links.withClass
 import org.jetbrains.dokka.model.DClass
+import org.jetbrains.dokka.model.DObject
 import org.jetbrains.dokka.model.KotlinVisibility
 import org.jetbrains.dokka.model.Visibility
 import org.jetbrains.dokka.model.doc.Description
@@ -36,8 +40,7 @@ fun typeToDClass(type: L_NamespaceMember_Type, sourceSet: DokkaConfiguration.Dok
         return DClass(
                 dri = dri,
                 name = simpleName.str,
-                documentation = mapOf(sourceSet to DocumentationNode(listOf(Description(Text(docSymbol.comment?.description
-                        ?: "Type $simpleName"))))),
+                documentation = mapOf(sourceSet to docSymbol.toDocumentationNode()),
 
                 constructors = constructors,
                 properties = listOf(),
@@ -53,5 +56,12 @@ fun typeToDClass(type: L_NamespaceMember_Type, sourceSet: DokkaConfiguration.Dok
                 sources = mapOf(sourceSet to DescriptorDocumentableSource(RellDeclarationDescriptor())),
                 modifier = mapOf()
         )
+    }
+
+    fun a(parent: SearchbarDataInstaller.DRIWithSourceSets) {
+
+        DObject(
+                dri = parent.dri.withClass("static"),
+                )
     }
 }
