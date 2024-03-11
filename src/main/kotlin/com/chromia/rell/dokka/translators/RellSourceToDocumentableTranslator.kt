95448cba0f88ec47064ac908b34d38ff9fc5ed2d
Joar Svartholm <joar.svartholm@chromaway.com>
2024-03-11 10:55:01 +0100

    Remove extensions.kt containing legacy translators

--- src/main/kotlin/com/chromia/rell/dokka/translators/RellSourceToDocumentableTranslator.kt
+++ src/main/kotlin/com/chromia/rell/dokka/translators/RellSourceToDocumentableTranslator.kt
@@ -4,19 +4,10 @@ package com.chromia.rell.dokka.translators
 
 import com.chromia.rell.dokka.RellDokkaPlugin
 import com.chromia.rell.dokka.config.RellDokkaPluginConfiguration
-import com.chromia.rell.dokka.model.definitionsByModule
-import com.chromia.rell.dokka.model.toDClasslike
-import com.chromia.rell.dokka.model.toDFunction
-import com.chromia.rell.dokka.model.toDProperty
-import com.chromia.rell.dokka.model.toDRI
 import net.postchain.rell.api.base.RellApiCompile
-import net.postchain.rell.base.model.R_App
 import org.jetbrains.dokka.DokkaConfiguration
 import org.jetbrains.dokka.model.DModule
-import org.jetbrains.dokka.model.DPackage
-import org.jetbrains.dokka.model.doc.Description
 import org.jetbrains.dokka.model.doc.DocumentationNode
-import org.jetbrains.dokka.model.doc.Text
 import org.jetbrains.dokka.plugability.DokkaContext
 import org.jetbrains.dokka.plugability.configuration
 import org.jetbrains.dokka.transformers.sources.SourceToDocumentableTranslator
@@ -40,29 +31,6 @@ class RellSourceToDocumentableTranslator(context: DokkaContext) : SourceToDocume
                     it,
                     mapOf(sourceSet to DocumentationNode(listOf())),
                     sourceSets = setOf(sourceSet),
-            )
-        }
-    }
-
-    private fun R_App.packages(sourceSet: DokkaConfiguration.DokkaSourceSet): List<DPackage> {
-        val defs = definitionsByModule()
-        return defs.map { (m, rellModule) ->
-            DPackage(
-                    dri = m.toDRI(),
-                    documentation = mapOf(sourceSet to DocumentationNode(listOf(Description(Text(m.docSymbol.comment?.toString()
-                            ?: ""))))),
-                    sourceSets = setOf(sourceSet),
-                    properties = rellModule.constants.map { it.toDProperty(sourceSet) }, // Global constants
-                    // Entities/Structs/Objects
-                    classlikes = rellModule.entities.map { it.toDClasslike(sourceSet) }
-                            + rellModule.objects.map { it.toDClasslike(sourceSet) }
-                            + rellModule.structs.map { it.toDClasslike(sourceSet) }
-                            + rellModule.enums.map { it.toDClasslike(sourceSet) },
-                    typealiases = listOf(),
-                    // Functions, queries, operations
-                    functions = rellModule.functions.map { it.toDFunction(sourceSet) }
-                            + rellModule.queries.map { it.toDFunction(sourceSet) }
-                            + rellModule.operations.map { it.toDFunction(sourceSet) }
             )
         }
     }
