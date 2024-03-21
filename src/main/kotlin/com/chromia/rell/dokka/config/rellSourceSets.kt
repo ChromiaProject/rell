b260bef819abe83256fd89e786cc01b944fbd9d0
Joar Svartholm <joar.svartholm@chromaway.com>
2024-03-21 11:51:29 +0100

    Transform includes files to support rell terminology

--- src/main/kotlin/com/chromia/rell/dokka/config/rellSourceSets.kt
+++ src/main/kotlin/com/chromia/rell/dokka/config/rellSourceSets.kt
@@ -4,12 +4,28 @@ import org.jetbrains.dokka.DokkaSourceSetID
 import org.jetbrains.dokka.DokkaSourceSetImpl
 import org.jetbrains.dokka.Platform
 import java.io.File
+import kotlin.io.path.writeLines
 
 fun rellSourceSets(projectRoot: File, includes: List<File>) = listOf(DokkaSourceSetImpl(
         sourceRoots = setOf(projectRoot),
         sourceSetID = DokkaSourceSetID("main", "dapp"),
         displayName = "dapp",
         analysisPlatform = Platform.wasm,
-        includes = includes.toSet(),
+        includes = includes.map { tranformModuleFile(it) }.toSet(),
         externalDocumentationLinks = setOf(systemLibExternalDocumentationLink)
 ))
+
+private fun tranformModuleFile(input: File): File {
+    return with(kotlin.io.path.createTempFile(prefix = "rell-dokka-plugin", suffix = "module-docs")) {
+        writeLines(
+                input.readLines().map {
+                    when {
+                        it.startsWith("# Dapp") -> "# Module" + it.substringAfter("# Dapp")
+                        it.startsWith("# Module") -> "# Package" + it.substringAfter("# Module")
+                        else -> it
+                    }
+                }
+        )
+        toFile()
+    }
+}
