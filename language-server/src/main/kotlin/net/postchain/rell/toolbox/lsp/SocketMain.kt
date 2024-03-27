2d28172c91b7c843bf0cf7b5a0618035506bfe4f
Mikheil Kezherashvili <misha1024@gmail.com>
2024-03-27 16:43:15 +0400

    Set locale programatically

--- language-server/src/main/kotlin/net/postchain/rell/toolbox/lsp/SocketMain.kt
+++ language-server/src/main/kotlin/net/postchain/rell/toolbox/lsp/SocketMain.kt
@@ -8,9 +8,12 @@ import net.postchain.rell.toolbox.util.initializeLogger
 import org.apache.logging.log4j.Level
 import org.koin.core.context.startKoin
 import org.koin.core.qualifier.named
+import java.util.Locale
 
 @ExcludeFromJacocoGeneratedReport
 fun main(args: Array<String>) {
+    Locale.setDefault(Locale.US)
+
     val logLevel = Level.INFO
     val launcherType = LauncherType.SOCKET
 
