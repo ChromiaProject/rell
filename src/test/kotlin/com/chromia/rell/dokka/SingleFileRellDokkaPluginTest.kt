61cc10d8216eb223e542095f9a58f346e5b4b068
Joar Svartholm <joar.svartholm@chromaway.com>
2024-04-10 15:31:17 +0200

    Set log level to WARN in tests

--- src/test/kotlin/com/chromia/rell/dokka/SingleFileRellDokkaPluginTest.kt
+++ src/test/kotlin/com/chromia/rell/dokka/SingleFileRellDokkaPluginTest.kt
@@ -5,12 +5,15 @@ import com.chromia.rell.dokka.config.systemLibExternalDocumentationLink
 import org.jetbrains.dokka.base.testApi.testRunner.BaseAbstractTest
 import org.jetbrains.dokka.base.testApi.testRunner.BaseTestBuilder
 import org.jetbrains.dokka.plugability.DokkaPlugin
+import org.jetbrains.dokka.testApi.logger.TestLogger
+import org.jetbrains.dokka.utilities.DokkaConsoleLogger
+import org.jetbrains.dokka.utilities.LoggingLevel
 import testApi.testRunner.DokkaSourceSetBuilder
 import testApi.testRunner.TestDokkaConfigurationBuilder
 
 const val TEST_DAPP_NAME = "test-dapp"
 
-abstract class SingleFileRellDokkaPluginTest : BaseAbstractTest() {
+abstract class SingleFileRellDokkaPluginTest : BaseAbstractTest(logger = TestLogger(DokkaConsoleLogger(LoggingLevel.WARN))) {
     private fun buildConfiguration(configurationBuilder: TestDokkaConfigurationBuilder.() -> Unit, sourceSetBuilder: DokkaSourceSetBuilder.() -> Unit) = dokkaConfiguration {
         pluginsConfigurations.add(RellDokkaPluginConfiguration(TEST_DAPP_NAME, listOf("main")).toPluginConfig())
         sourceSets {
