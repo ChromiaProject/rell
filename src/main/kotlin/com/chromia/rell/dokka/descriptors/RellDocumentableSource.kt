5060535bad179004b18e30c31af3a8513a48060c
Joar Svartholm <joar.svartholm@chromaway.com>
2024-04-10 11:30:29 +0200

    Remove unused classes

--- src/main/kotlin/com/chromia/rell/dokka/descriptors/RellDocumentableSource.kt
+++
@@ -1,7 +0,0 @@
-@file:Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
-package com.chromia.rell.dokka.descriptors
-
-import com.chromia.rell.dokka.doc.RellDocumentableSource
-import org.jetbrains.dokka.model.DocumentableSource
-
-val NULL_DESCRIPTOR: DocumentableSource = RellDocumentableSource()
