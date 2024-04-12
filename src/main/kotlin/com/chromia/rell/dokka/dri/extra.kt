e780b5a9d34325709ee45a07e7f18e5ac1b38bdc
Tim Steinholtz <tim.steinholtz@chromaway.com>
2024-04-12 11:26:02 +0200

    Updates creation of DRI to use defName instead of cDefName

--- src/main/kotlin/com/chromia/rell/dokka/dri/extra.kt
+++ src/main/kotlin/com/chromia/rell/dokka/dri/extra.kt
@@ -1,5 +1,6 @@
 package com.chromia.rell.dokka.dri
 
+import net.postchain.rell.base.model.R_DefinitionName
 import org.jetbrains.dokka.links.DRI
 import org.jetbrains.dokka.links.DRIExtraContainer
 import org.jetbrains.dokka.links.DRIExtraProperty
@@ -10,3 +11,7 @@ fun DRI.isAlias() = DRIExtraContainer(extra)[AliasDRIExtra] != null
 object AliasDRIExtra : DRIExtraProperty<AliasDRIExtra>()
 
 fun String.escapeAnonymousFunctionName() = replace("function#", "function%23")
+
+fun R_DefinitionName.toPackageName() =
+        (listOf(module) + qualifiedName.substringBeforeLast(".", ""))
+            .joinToString(".").trimEnd('.')
