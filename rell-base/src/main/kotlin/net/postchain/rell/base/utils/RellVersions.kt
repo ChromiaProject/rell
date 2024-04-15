6ff01435d8fda2181627921134d8cefe938c1ad5
Anton Karmanov <antkar.dev@gmail.com>
2024-04-15 21:26:49 +0000

    Rell 0.13.11

--- rell-base/src/main/kotlin/net/postchain/rell/base/utils/RellVersions.kt
+++ rell-base/src/main/kotlin/net/postchain/rell/base/utils/RellVersions.kt
@@ -7,7 +7,7 @@ package net.postchain.rell.base.utils
 import net.postchain.rell.base.model.R_LangVersion
 
 object RellVersions {
-    const val VERSION_STR = "0.14.0"
+    const val VERSION_STR = "0.13.11"
     val VERSION = R_LangVersion.of(VERSION_STR)
 
     val SUPPORTED_VERSIONS: Set<R_LangVersion> =
@@ -21,8 +21,7 @@ object RellVersions {
                 "0.11.0",
                 "0.12.0",
                 "0.13.0", "0.13.1", "0.13.2", "0.13.3", "0.13.4", "0.13.5", "0.13.6", "0.13.7", "0.13.8", "0.13.9",
-                "0.13.10",
-                "0.14.0",
+                "0.13.10", "0.13.11",
             )
             .map { R_LangVersion.of(it) }
             .toImmSet()
