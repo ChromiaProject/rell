13ece5b4965dec0d334f0f2a15d6739409c1a070
Anton Karmanov <antkar.dev@gmail.com>
2024-04-15 10:50:51 +0000

    Maximum entity and object mount name length changed to 58

--- rell-base/src/main/kotlin/net/postchain/rell/base/compiler/ast/s_def.kt
+++ rell-base/src/main/kotlin/net/postchain/rell/base/compiler/ast/s_def.kt
@@ -423,7 +423,7 @@ class S_EntityDefinition(
         )
 
         private val NAME_LEN_SWITCH = C_FeatureSwitch("0.12.0")
-        private const val MAX_ENTITY_MOUNT_NAME_LEN = 60 // Postgres allows 63, minimal prefix is "c0.", i. e. 3 characters.
+        private const val MAX_ENTITY_MOUNT_NAME_LEN = 58 // Postgres allows 63, minimal prefix is "c0.", i. e. 3 characters.
         private const val MAX_ATTR_NAME_LEN = 63
 
         fun checkEntityMountNameLen(msgCtx: C_MessageContext, name: C_Name, mountName: R_MountName) {
