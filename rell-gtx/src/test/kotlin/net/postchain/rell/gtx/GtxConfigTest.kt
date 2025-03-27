d9c7f7382d542962bddb0f771d98b4a9c5fcffca
Anton Karmanov <antkar.dev@gmail.com>
2025-03-27 16:14:11 +0000

    Rell 0.14.8

--- rell-gtx/src/test/kotlin/net/postchain/rell/gtx/GtxConfigTest.kt
+++ rell-gtx/src/test/kotlin/net/postchain/rell/gtx/GtxConfigTest.kt
@@ -109,6 +109,7 @@ class GtxConfigTest: BaseGtxTest() {
         chkVersion("0.14.5", "OK")
         chkVersion("0.14.6", "OK")
         chkVersion("0.14.7", "OK")
+        chkVersion("0.14.8", "OK")
 
         chkVersion("0.15.0", "OK")
     }
@@ -144,7 +145,7 @@ class GtxConfigTest: BaseGtxTest() {
         chkVersion("0.11.1", err)
         chkVersion("0.12.1", err)
         chkVersion("0.13.16", err)
-        chkVersion("0.14.8", err)
+        chkVersion("0.14.9", err)
         chkVersion("0.15.1", err)
         chkVersion("1.0.0", err)
     }
