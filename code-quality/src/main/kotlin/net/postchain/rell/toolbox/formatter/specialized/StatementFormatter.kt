bdfabe29f8d05a5fdfecebe065c0ec863734842e
Tim Steinholtz <tim.steinholtz@chromaway.com>
2025-09-11 15:46:20 +0200

    Small formatter update for new language annotations

--- code-quality/src/main/kotlin/net/postchain/rell/toolbox/formatter/specialized/StatementFormatter.kt
+++ code-quality/src/main/kotlin/net/postchain/rell/toolbox/formatter/specialized/StatementFormatter.kt
@@ -17,6 +17,10 @@ class BlockStmtFormatter : NodeFormatter<RuleX_BlockStmtContext> {
         doc.prepend(xBlockStmt.ruleX_tkLCURL()) { it.oneSpace() }
         val statements = xBlockStmt.ruleX_StatementRef()
 
+        if (statements.size == 0 ) {
+            doc.append(xBlockStmt.ruleX_tkLCURL()) { it.noSpace() }
+        }
+
         statements.forEachIndexed { index, statement ->
             doc.prepend(statement) {
                 it.setNewLines(1, 1, 2)
