060185879011ed3c2619c53463f343c943443807
Mikheil Kezherashvili <misha1024@gmail.com>
2025-09-11 16:29:30 +0400

    Update RellResourceFactory.kt

--- indexer/src/main/kotlin/net/postchain/rell/toolbox/indexer/RellResourceFactory.kt
+++ indexer/src/main/kotlin/net/postchain/rell/toolbox/indexer/RellResourceFactory.kt
@@ -24,6 +24,8 @@ import org.antlr.v4.runtime.TokenStream
 import java.io.File
 import java.net.URI
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.reflect.full.declaredMemberFunctions
+import kotlin.reflect.jvm.isAccessible
 
 class RellResourceFactory(
     private val workspaceUri: URI,
@@ -98,9 +100,10 @@ class RellResourceFactory(
 
     fun S_RellFile.ideModuleInfoByReflection(sourcePath: C_SourcePath): IdeModuleInfo? {
         return try {
-            val method = S_RellFile::class.java.getDeclaredMethod("ideModuleInfo", C_SourcePath::class.java)
-            method.isAccessible = true
-            method.invoke(this, sourcePath) as? IdeModuleInfo
+            S_RellFile::class.declaredMemberFunctions.find { it.name == "ideModuleInfo" }?.let {
+                it.isAccessible = true
+                return it.call(this, sourcePath) as? IdeModuleInfo
+            }
         } catch (e: Exception) {
             logger.warn(e) { "Failed to call ideModuleInfo by reflection for ${sourcePath.str()}" }
             null
